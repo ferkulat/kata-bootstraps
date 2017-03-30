16eec76c350f134c8e67a5a9fd200ae7a755febe
Marcel Schneider <marcel@coopmasters.de>
2017-03-30 22:05:30 +0200

    starting point for cpp with cmake and testing frame work "catch"

---
+++ cpp/catch/Tests/TestMain.cpp
@@ -0,0 +1,7 @@
+//
+// Created by marcel on 3/30/17.
+//
+#define CATCH_CONFIG_MAIN
+
+#include "catch.hpp"
+
---
+++ cpp/catch/Tests/Test_1.cpp
@@ -0,0 +1,9 @@
+//
+// Created by marcel on 3/30/17.
+//
+#include "catch.hpp"
+
+TEST_CASE("First failling test")
+{
+    REQUIRE( 2 == 1 );
+}
